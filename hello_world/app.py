0c3fe51ac6faaf7b21b5e1f05ec675408c57df79
DevRico003 <carteciano.enrico@gmail.com>
2023-12-04 15:54:22 +0100

    templates/lambda changes

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,11 +1,13 @@
 import json
 
 def lambda_handler(event, context):
+    print("Change deployed with SAM Accelerate");
     return {
     "statusCode": 200,
     "body": json.dumps({
         "message": "I'm using canary deployments",
     }),
 }
+
 
 
